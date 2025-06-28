5274c892e51a6b2c201afa5c8a3821684c964db0
Edson Junior <edsonkjr@gmail.com>
2025-06-27 22:06:05 -0300

    feat(init):Creating base project configurations

---
+++ src/main/java/com/agendr/app/AgendrApplication.java
@@ -0,0 +1,13 @@
+package com.agendr.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agendr/app/AgendrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendr.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
